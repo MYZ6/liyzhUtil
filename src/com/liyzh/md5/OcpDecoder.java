4d34bd318c27a3bcea6e999994b171b4a5823931
liyzh <lyz19890927@163.com>
2013-08-21 16:21:45 +0800

    根据登录名查询密码，然后由密文得到明文

--- src/com/liyzh/md5/OcpDecoder.java
+++ src/com/liyzh/md5/OcpDecoder.java
@@ -3,6 +3,11 @@ package com.liyzh.md5;
 import java.security.Key;
 import java.security.SecureRandom;
 import java.security.Security;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
@@ -45,7 +50,56 @@ public class OcpDecoder {
 
 	public static void main(String[] args) {
 		OcpDecoder decoder = new OcpDecoder();
-		String result = decoder.decryptPassword("E0r/eBP8WjM=");
+		String result = decoder.decryptPassword(getPassword("11911944"));
 		System.out.println(result);
 	}
+
+	private static String getPassword(String loginname) {
+		try {
+			Class.forName("com.ibm.db2.jcc.DB2Driver").newInstance();
+		} catch (InstantiationException e1) {
+			e1.printStackTrace();
+		} catch (IllegalAccessException e1) {
+
+			e1.printStackTrace();
+		} catch (ClassNotFoundException e1) {
+
+			e1.printStackTrace();
+		}
+		String url153 = "jdbc:db2://10.157.33.153:50000/FMS";
+		String url48 = "jdbc:db2://10.157.33.48:50000/FMS";
+		String url = "jdbc:db2://localhost:50000/FMS";
+		String user = "FMS";
+		String password = "Fms123";
+		String password48 = "FMS123";
+
+		String pwd = "";
+		try {
+			Connection conn = DriverManager.getConnection(url48, user,
+					password48);
+			String sqlStr = "select * from FMS.ORGMODEL_USER u where u.LOGINNAME = '"
+					+ loginname + "'";
+			Statement st = conn.createStatement();
+			System.out.println("创建Statement成功!");
+
+			ResultSet rs = st.executeQuery(sqlStr);
+			System.out.println("操作数据表成功!");
+			System.out.println("----------------!");
+			while (rs.next()) {
+				pwd = rs.getString("PASSWORD");
+				System.out.print(rs.getString("LOGINNAME") + "     ");
+				System.out.print(rs.getString("PASSWORD") + "     ");
+				System.out.println(rs.getString("NAME"));
+				break;
+			}
+			rs.close();
+			st.close();
+			conn.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return pwd;
+	}
+
 }
